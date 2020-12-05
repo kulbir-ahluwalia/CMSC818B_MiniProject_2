5669f6d517c0a591d94935c5c361801accf31d7e
Vishnu Dutt Sharma <vishnusharma619@gmail.com>
2020-12-05 18:54:47 -0500

    Turned off render. Added shared reward parameter

--- python/constants.py
+++ python/constants.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-RENDER_PYGAME = True
+RENDER_PYGAME = False
 
 
 ## Drone Constants
@@ -8,9 +8,11 @@ class CONSTANTS:
     def __init__(self):
         self.TIME_STEP = 1
 
-        self.NUM_AGENTS = 4
+        self.NUM_AGENTS = 1
 
         self.LEN_EPISODE = 1000
+
+        self.isSharedReward = True
 
 
 
