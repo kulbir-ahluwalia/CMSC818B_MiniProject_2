9f147c9f9b4e28aeb92ff254efbdff47a41b74f5
Vishnu Dutt Sharma <vishnuds@umd.edu>
2020-12-12 16:12:14 -0500

    Changed the intial step size

--- python/actors.py
+++ python/actors.py
@@ -3,12 +3,15 @@ import pygame.gfxdraw
 import numpy as np
 
 class Player():
-    def __init__(self, pos, color='g', size=5, step_size=1):
+    def __init__(self, pos, color='g', size=5, step_size=None):
         self.pos = np.array(pos)
         self.color = color 
         self.size = size
         self.prev_action = None
-        self.step_size = step_size
+        if step_size is None:
+            self.step_size = self.size
+        else:
+            self.step_size = step_size
         
         self.COLOR = None
         if self.color == 'r':
